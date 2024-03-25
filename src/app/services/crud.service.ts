b71a4c3b668658d1d57b235db1c3ff1137ea1ac2
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-03-25 20:41:22 +0100

    Fix payload passing for create method, add missing generics.

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -6,13 +6,13 @@ import { Injectable } from "@angular/core";
 export interface ICRUDService {
   getAll: <T>(key: string) => Observable<T[]>;
   getOne: <T>(key: string, id: number) => Observable<T>;
-  create: <T>(key: string, payload: Partial<T>) => Observable<number>;
+  create: <T>(key: string, payload: Partial<T>) => Observable<T>;
   update: <T>(
     key: string,
     id: number,
     payload: Partial<T>,
-  ) => Observable<number>;
-  delete: (key: string, id: number) => Observable<number>;
+  ) => Observable<T>;
+  delete: <T>(key: string, id: number) => Observable<T>;
 }
 
 @Injectable()
@@ -34,25 +34,22 @@ export class CRUDService implements ICRUDService {
     return this.client.get<T>(`${this.baseUrl}/${key}/${id}`);
   }
   create<T>(key: string, payload: Partial<T>) {
-    const options = {
-      ...this.httpOptions,
-      body: JSON.stringify(payload),
-    };
-    return this.client.post<number>(`${this.baseUrl}/${key}`, options);
-  }
-  update<T>(key: string, id: number, payload: Partial<T>) {
-    const options = {
-      ...this.httpOptions,
-    };
-    return this.client.put<number>(
-      `${this.baseUrl}/${key}/${id}`,
+    return this.client.post<T>(
+      `${this.baseUrl}/${key}`,
       payload,
-      options,
+      this.httpOptions,
     );
   }
-  delete(key: string, id: number) {
+  update<T>(key: string, id: number, payload: Partial<T>) {
+    return this.client.put<T>(
+      `${this.baseUrl}/${key}/${id}`,
+      payload,
+      this.httpOptions,
+    );
+  }
+  delete<T>(key: string, id: number) {
     console.log("Sending request...");
     console.log(key, id);
-    return this.client.delete<number>(`${this.baseUrl}/${key}/${id}`);
+    return this.client.delete<T>(`${this.baseUrl}/${key}/${id}`);
   }
 }
