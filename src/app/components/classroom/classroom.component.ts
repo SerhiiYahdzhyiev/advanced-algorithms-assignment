3b7b322489a2756cbaecf99f66ab3a16d16d3bc4
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-03-26 17:21:26 +0100

    Develop classroom component.

--- src/app/components/classroom/classroom.component.ts
+++ src/app/components/classroom/classroom.component.ts
@@ -1,12 +1,76 @@
-import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+
 import { Classroom } from "@interfaces";
+
+import { CRUDService } from "@services";
 
 @Component({
   standalone: true,
   selector: "classroom",
   templateUrl: "./classroom.component.html",
+  styleUrl: "./classroom.component.css",
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  providers: [
+    CRUDService,
+  ],
 })
-export class ClassroomComponent {
+export class ClassroomComponent implements OnInit {
   @Input()
-  classroom: Classroom | null = null;
+  classroom: Classroom = {} as Classroom;
+
+  isEditing: boolean = false;
+
+  initialTitle: string = "";
+  initialCapacity: number = 0;
+
+  ngOnInit() {
+    this.initialTitle = this.classroom.title;
+    this.initialCapacity = this.classroom.capacity;
+  }
+
+  constructor(private crudService: CRUDService) { }
+
+  startEditing() {
+    this.isEditing = true;
+  }
+
+  endEditing() {
+    this.isEditing = false;
+  }
+
+  save() {
+    this.crudService.update<Classroom>(
+      "classrooms",
+      this.classroom.id,
+      this.classroom,
+    ).subscribe();
+
+    this.initialCapacity = this.classroom.capacity;
+    this.initialTitle = this.classroom.title;
+
+    this.endEditing();
+  }
+
+  handleCancel() {
+    this.classroom.title = this.initialTitle;
+    this.classroom.capacity = this.initialCapacity;
+    this.endEditing();
+  }
+
+  remove() {
+    this.crudService.delete<Classroom>("classrooms", this.classroom.id)
+      .subscribe(() => window.location.reload());
+  }
 }
