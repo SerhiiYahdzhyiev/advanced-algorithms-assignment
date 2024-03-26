a70d14cc987d9f70a15bc2f06aa8c13a09492aa1
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-03-26 17:22:17 +0100

    Develoop lecture component.

--- src/app/components/lecture/lecture.component.ts
+++ src/app/components/lecture/lecture.component.ts
@@ -1,12 +1,80 @@
-import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+
 import { Lecture } from "@interfaces";
+
+import { CRUDService } from "@services";
 
 @Component({
   standalone: true,
   selector: "lecture",
   templateUrl: "./lecture.component.html",
+  styleUrl: "./lecture.component.css",
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  providers: [
+    CRUDService,
+  ],
 })
-export class LectureComponent {
+export class LectureComponent implements OnInit {
   @Input()
-  lecture: Lecture | null = null;
+  lecture: Lecture = {} as Lecture;
+
+  isEditing: boolean = false;
+
+  initialTitle: string = "";
+  initialParticipants: number = 0;
+  initialDuration: number = 0;
+
+  ngOnInit() {
+    this.initialTitle = this.lecture.title;
+    this.initialParticipants = this.lecture.participantsCount;
+    this.initialDuration = this.lecture.duration;
+  }
+
+  constructor(private crudService: CRUDService) { }
+
+  startEditing() {
+    this.isEditing = true;
+  }
+
+  endEditing() {
+    this.isEditing = false;
+  }
+
+  save() {
+    this.crudService.update<Lecture>(
+      "lectures",
+      this.lecture.id,
+      this.lecture,
+    ).subscribe();
+
+    this.initialDuration = this.lecture.duration;
+    this.initialParticipants = this.lecture.participantsCount;
+    this.initialTitle = this.lecture.title;
+
+    this.endEditing();
+  }
+
+  handleCancel() {
+    this.lecture.title = this.initialTitle;
+    this.lecture.duration = this.initialDuration;
+    this.lecture.participantsCount = this.initialParticipants;
+    this.endEditing();
+  }
+
+  remove() {
+    this.crudService.delete<Lecture>("lectures", this.lecture.id)
+      .subscribe(() => window.location.reload());
+  }
 }
